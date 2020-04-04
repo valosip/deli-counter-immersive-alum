c8cfc354411f4d27b8a08ee215a8accd413423f3
Val Osip <valosip@gmail.com>
2020-04-04 23:21:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,13 @@
 # Write your code here.
 
 def line(array)
-  if array.size == 0 
-    "The line is currently empty."
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
   end
 end
